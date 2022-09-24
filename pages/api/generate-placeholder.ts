badf4622a73ed8dd4cb175ed73ce5b136692b152
dukesx <afzaalbci@ymail.com>
2022-09-24 11:14:52 +0500

    fixes
    Signed-off-by: dukesx <afzaalbci@ymail.com>

--- pages/api/generate-placeholder.ts
+++ pages/api/generate-placeholder.ts
@@ -1,22 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
-import { getPlaiceholder } from "plaiceholder";
+// import { getPlaiceholder } from "plaiceholder";
 
 export default async function generatePlaceholder(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const { cover } = req.body;
-    const { base64 } = await getPlaiceholder(
-      `https://ik.imagekit.io/afrididotdev/tr:w-400${cover}`
-    );
-    return res.status(200).json({
-      placeholder: base64,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      error: err,
-    });
-  }
+  // try {
+  //   const { cover } = req.body;
+  //   const { base64 } = await getPlaiceholder(
+  //     `https://ik.imagekit.io/afrididotdev/tr:w-400${cover}`
+  //   );
+  //   return res.status(200).json({
+  //     placeholder: base64,
+  //   });
+  // } catch (err) {
+  //   return res.status(500).json({
+  //     error: err,
+  //   });
+  // }
 }
