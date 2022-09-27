feeabb14582b1efbc206d5391daf941e53b49658
dukesx <afzaalbci@ymail.com>
2022-09-27 18:35:02 +0500

    fixes
    Signed-off-by: dukesx <afzaalbci@ymail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,7 @@ const config: PlaywrightTestConfig = {
   testDir: "./e2e",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
+
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -34,6 +35,7 @@ const config: PlaywrightTestConfig = {
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
