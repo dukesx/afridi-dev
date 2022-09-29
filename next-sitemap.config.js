17f8884ffd48b75b43c07a89fef43862f82e1daa
dukesx <afzaalbci@ymail.com>
2022-09-29 12:11:07 +0500

    fixes
    Signed-off-by: dukesx <afzaalbci@ymail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://localhost:3000",
+  siteUrl: "https://afridi.dev",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
+  exclude: ["/server-sitemap.xml"],
   robotsTxtOptions: {
     additionalSitemaps: [
       "https://afridi.dev/server-sitemap.xml", // <==== Add here
